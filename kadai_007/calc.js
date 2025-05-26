8f4efccb95b019be5102cde4ab8a4d2fbcb7f5b5
yu-pi9 <yuuu5141@outlook.jp>
2025-05-26 18:29:36 +0900

    jsの再提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,12 @@
+//変数の宣言・値の代入
+let num = Math.floor(Math.random() * 49) + 1
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}else{
+    console.log(num)
+}
